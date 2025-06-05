aa0b11647bd82c4e97b4ee00aa887e555135359c
Markel Cortajarena <markelkortajarena@gmail.com>
2025-06-05 13:40:36 +0200

    add bcrypt and jwt imports to users.controller.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,13 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/users.model');
 
 const getAll = async (req, res) => {
     const users = await User.selectAll();
     res.json(users);
 }
+
+
 
 module.exports = { getAll }
