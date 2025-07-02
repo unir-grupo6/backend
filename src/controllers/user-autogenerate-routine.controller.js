42499ecca551e39a4faeef1b5d8d112b5262179e
gimhara <gimhara@hotmail.com>
2025-07-02 13:22:30 +0200

    Finalizado hasta sugerir rutina aleatoria no realizada

--- src/controllers/user-autogenerate-routine.controller.js
+++ src/controllers/user-autogenerate-routine.controller.js
@@ -1,14 +1,58 @@
 const auto = require('../models/user-autogenerate-routine.model');
 
+let usuarioObjetivos = {
+    idusuario: 0,
+    objetivo: 0,
+    rutinas_realizadas: []    
+}
+
+
 const autoGenerate = async (req, res) => {
+
+    let usuarioObjetivos = {
+        idusuario: 0,
+        objetivo: 0,
+        rutinas_realizadas: []    
+    };
+
+    let rutinasIds;
+    let result;
+
     try{
         const id = Number(req.params.id);
         if (isNaN(id)) {
             return res.status(400).json({ message: 'Invalid idUser' });
         }
 
-        const result = await auto.autoGenerate(id);
-        console.log(result, ' - VALOR DE RESULTADO EN CONTROLADOR');        
+        result = await auto.objetivosUsuario(id);
+
+        usuarioObjetivos.idusuario = id;
+        usuarioObjetivos.objetivo = result[0].id_objetivos;        
+        
+        result = await auto.rutinasRealizadas(id);
+
+        // Añadir el array result a rutinas_realizadas
+        rutinasIds = result.map(obj => obj.rutinas_id);
+        usuarioObjetivos.rutinas_realizadas = rutinasIds;
+        
+        result = await auto.rutinasAutogeneradas(id);
+        
+        
+        // Extraer los IDs de las rutinas autogeneradas y filtrar duplicados
+        const rutinasAutogeneradasIds = result.map(obj => obj.idrutina);
+        const nuevosIds = rutinasAutogeneradasIds.filter(id => !rutinasIds.includes(id));
+        rutinasIds.push(...nuevosIds);
+        
+        // Actualizar el objeto con todos los IDs combinados
+        usuarioObjetivos.rutinas_realizadas = rutinasIds;        
+
+        result = await auto.rutinaSugerida(usuarioObjetivos);
+        console.log(result, ' - RUTINA SUGERIDA EN CONTROLADOR');
+
+        if (!result) {
+            return res.status(499).json({ message: 'No se han sugerido rutinas' });
+        }
+        
         return res.status(200).json(result);
     
     }
